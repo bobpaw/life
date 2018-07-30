bbfad59ce340ccdca9771c1b35a95e19485478e2
Aiden Woodruff <aiden.woodruff@gmail.com>
2018-07-29 20:37:12 -0500

    Added GPLv3 boilerplate to .c and .h files
    Added news for 1.5.0
    
    Signed-off-by: Aiden Woodruff <aiden.woodruff@gmail.com>

--- life.c
+++ life.c
@@ -1,33 +1,33 @@
 /*
 
-  * An arbitrary cellular automata model using ncurses
-  * Copyright (C) 2018 Aiden Woodruff
+ * An arbitrary cellular automata model using ncurses
+ * Copyright (C) 2018 Aiden Woodruff
 
-  * This program is free software: you can redistribute it and/or modify
-  * it under the terms of the GNU General Public License as published by
-  * the Free Software Foundation, either version 3 of the License, or
-  * (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
 
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
 
-  * You should have received a copy of the GNU General Public License
-  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-  */
+ */
 #include "life.h"
 
 #ifndef stat_bar_print
 // Print text to status bar in the nice way, without erasing or refreshing
-#define stat_bar_print(win, fmt,...)                                     \
+#define stat_bar_print(win, fmt,...)                                    \
   wprintw(win, fmt, ##__VA_ARGS__);                                     \
   for (int _i = getcurx(win); _i < COLS; _i++) waddch(win, ' ');
 #endif
 
 #ifndef foreach
-#define foreach(func, count, first,...)                           \
+#define foreach(func, count, first,...)                         \
   {typeof(first) things[] = {__VA_ARGS__};                      \
     func(first);                                                \
     for (int _i = 0; _i < count - 1; _i++) func(things[_i]);}
