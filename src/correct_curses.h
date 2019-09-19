f64ebb50da3a74b9060f1616d05b1e25e0503798
Aiden Woodruff <aiden.woodruff@gmail.com>
2019-09-18 22:51:25 -0500

    Use CMake instead of other stuff
    Add stuff to develop in Visual Studio (i.e. to gitignore)
    Add correct_curses.h
    Add MSVC fixes to foreach.h macros
    Add Windows getopt.c and getopt.h (for now).

---
+++ src/correct_curses.h
@@ -0,0 +1,24 @@
+#ifndef GRAPH_CORRECT_CURSES_H
+#define GRAPH_CORRECT_CURSES_H
+#if defined(HAVE_CMAKECONFIG_H)
+#include <cmakeconfig.h>
+
+#if defined HAVE_NCURSESW_CURSES_H
+#  include <ncursesw/curses.h>
+#elif defined HAVE_NCURSESW_H
+#  include <ncursesw.h>
+#elif defined HAVE_NCURSES_CURSES_H
+#  include <ncurses/curses.h>
+#elif defined HAVE_NCURSES_H
+#  include <ncurses.h>
+#elif defined HAVE_CURSES_H
+#  include <curses.h>
+#else
+#  error "SysV or X/Open-compatible Curses header file required"
+#endif
+#else
+
+// Fingers-crossed
+#include <curses.h>
+#endif
+#endif // GRAPH_CORRECT_CURSES_H
--- src/foreach.h
+++ src/foreach.h
@@ -1,11 +1,35 @@
 #ifndef NARG
-#define NARG(...) NARG_(__VA_ARGS__, RSEQ_N)
-#define NARG_(...) NARG_N(__VA_ARGS__)
-#define RSEQ_N 8, 7, 6, 5, 4, 3, 2, 1, 0
 #define NARG_N(_1, _2, _3, _4, _5, _6, _7, _8, N, ...) N
+#define RSEQ_N 8, 7, 6, 5, 4, 3, 2, 1, 0
+#if _MSC_VER && !__INTEL_COMPILER
+#define EXPAND(...) __VA_ARGS__
+#define NARG_(...) EXPAND(EXPAND(NARG_N) (__VA_ARGS__))
+#else
+#define NARG_(...) NARG_N(__VA_ARGS__)
+#endif
+#define NARG(...) NARG_(__VA_ARGS__, RSEQ_N)
 #endif
 
 #ifndef foreach
+
+#define CONCAT2(x, y) x##y
+#define CONCAT1(x, y) CONCAT2(x,y)
+#define CONCATENATE(x, y) CONCAT1(x, y)
+
+#if _MSC_VER && !__INTEL_COMPILER
+#undef EXPAND
+#define EXPAND(...) __VA_ARGS__
+#define FOREACH_0(func) } while (0)
+#define FOREACH_1(func, first) func(first); FOREACH_0(func)
+#define FOREACH_2(func, first,...) func(first); EXPAND(EXPAND(FOREACH_1)(func, __VA_ARGS__))
+#define FOREACH_3(func, first,...) func(first); EXPAND(EXPAND(FOREACH_2)(func, __VA_ARGS__))
+#define FOREACH_4(func, first,...) func(first); EXPAND(EXPAND(FOREACH_3)(func, __VA_ARGS__))
+#define FOREACH_5(func, first,...) func(first); EXPAND(EXPAND(FOREACH_4)(func, __VA_ARGS__))
+#define FOREACH_6(func, first,...) func(first); EXPAND(EXPAND(FOREACH_5)(func, __VA_ARGS__))
+#define FOREACH_7(func, first,...) func(first); EXPAND(EXPAND(FOREACH_6)(func, __VA_ARGS__))
+#define FOREACH_8(func, first,...) func(first); EXPAND(EXPAND(FOREACH_7)(func, __VA_ARGS__))
+#define foreach(func, ...) do { EXPAND(CONCATENATE(FOREACH_, NARG(__VA_ARGS__))(func, __VA_ARGS__))
+#else
 #define FOREACH_0(func) } while (0)
 #define FOREACH_1(func, first) func(first); FOREACH_0(func)
 #define FOREACH_2(func, first,...) func(first); FOREACH_1(func, __VA_ARGS__)
@@ -15,10 +39,7 @@
 #define FOREACH_6(func, first,...) func(first); FOREACH_5(func, __VA_ARGS__)
 #define FOREACH_7(func, first,...) func(first); FOREACH_6(func, __VA_ARGS__)
 #define FOREACH_8(func, first,...) func(first); FOREACH_7(func, __VA_ARGS__)
-
-#define CONCAT1(x, y) x##y
-#define CONCATENATE(x, y) CONCAT1(x, y)
-
-#define foreach(func, ...) do { CONCATENATE(FOREACH_,NARG(__VA_ARGS__))(func, __VA_ARGS__)
+#define foreach(func, ...) do { CONCATENATE(FOREACH_, NARG(__VA_ARGS__))(func, __VA_ARGS__)
+#endif
 #endif
 
